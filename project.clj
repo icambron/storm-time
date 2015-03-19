70e5ab87ceaafce3f54c30e792250cebef71a05b
Isaac Cambron <isaac@isaaccambron.com>
2015-03-19 14:29:06 -0400

    making it fail by default

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
   :uberjar :all
 
   :dependencies [[clj-time "0.8.0"]
-                 ;;[org.apache.storm/storm-core "0.9.3"]
+                 [org.apache.storm/storm-core "0.9.3"]
                  [postgresql/postgresql "9.3-1102.jdbc4"]
                  [org.clojure/java.jdbc "0.3.5"]])
